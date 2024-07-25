e752cc76263a0cf03f5cd11fd7cad9c80449b29e
shindesru1910 <119733618+shindesru1910@users.noreply.github.com>
2024-07-25 12:26:48 +0530

    First Commit Portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm Srushti Shinde, a web developer specialized in React.js.</p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <div className="project">
+        <h3>The Online Auction System</h3>
+        <p>Developed an Online Auction System as a part of a coursework project. The system aimed to provide a
+secure and user-friendly platform for users to buy and sell items through auctions.
+Role: Project Lead and Lead Developer
+Key Features: Implemented user registration, item listing, real-time bidding,User Management, and auction
+management.
+Achievements: Successfully created a fully functional online auction platform, which received positive feedback
+from both users and instructors.
+Github Links : 
+https://github.com/shindesru1910/online_auction_backend
+https://github.com/shindesru1910/online-auction-frontend</p>
+      </div>
+      <div className="project">
+        <h3>MyHealthMate</h3>
+        <p>MyHealthMate is a digital platform designed to enhance your health journey. It offers personalized health recommendations, a database of doctor information, easy appointment booking, health reports, and exercise tracking. Tailored for patients and admins, it streamlines healthcare management.
+
+Key Features:
+
+Personalized Recommendations: Custom advice on nutrition and lifestyle.
+Doctor Information: Detailed profiles and patient reviews.
+Appointment Booking: Easy scheduling with reminders.
+Health Reports: Secure storage and sharing of medical records.
+Exercise Tracking: Log workouts and set fitness goals.
+Premium Features: Discounts on consultations and exclusive content.
+Why Choose MyHealthMate?
+Comprehensive care, user-friendly design, reliable information, and community support.
+
+Join MyHealthMate today to take control of your health! For more details, visit www.myhealthmate.com.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
